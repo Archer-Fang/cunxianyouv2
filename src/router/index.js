22a9f253968305767db4f0a86cf218f3c10e83af
Archer <1091053002@qq.com>
2018-04-06 21:47:00 +0800

    添加钩子函数，给首页进行登陆认证，如果未登录，点击首页转到登陆页，否则转到首页

--- src/router/index.js
+++ src/router/index.js
@@ -27,9 +27,9 @@ const router= new Router({
       path: '/index',
       name: 'index',
       //身份验证
-     /* meta: {
+      meta: {
         requireAuth: true,  // 添加该字段，表示进入这个路由是需要登录的
-      },*/
+      },
       component: index,
     },
     {
